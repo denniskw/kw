c1b000784c6613c666a39d91200d8a199c35b8c8
Kang Wei <kangwei@gunlei.com>
2016-05-25 12:48:09 +0800

    tijiao

--- gunDemo/app/src/main/java/gundemo/gun/com/gundemo/MainActivity.java
+++ gunDemo/app/src/main/java/gundemo/gun/com/gundemo/MainActivity.java
@@ -14,13 +14,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace with your own action!", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
