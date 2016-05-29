7bee1a76adec0d6988520d819585c3eddcdc59b3
unknown <denniskw@163.com>
2016-05-29 23:52:57 +0800

    PullToRefresh

---
+++ gunDemo/app/src/main/java/gundemo/gun/com/gundemo/activity/PhotoViewBrowserActivity.java
@@ -0,0 +1,20 @@
+package gundemo.gun.com.gundemo.activity;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+
+import gundemo.gun.com.gundemo.R;
+
+
+public class PhotoViewBrowserActivity extends ViewPager {
+    public PhotoViewBrowserActivity(Context context) {
+        super(context);
+    }
+
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_photo_view_browser);
+//    }
+}
